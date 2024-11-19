204b2eb44b4840ee432b3d89ebb0e5ed23e18c20
theonlywisdom <WABaah@edinst.com>
2024-11-19 19:43:30 +0000

    adadd idataservice

---
+++ FriendStorage.DataAccess/IDataService.cs
@@ -0,0 +1,15 @@
+﻿using FriendStorage.Model;
+
+namespace FriendStorage.DataAccess
+{
+    public interface IDataService : IDisposable
+    {
+        Friend GetFriendById(int friendId);
+
+        void SaveFriend(Friend friend);
+
+        void DeleteFriend(int friendId);
+
+        IEnumerable<Friend> GetAllFriends();
+    }
+}
