fc8beba12ae2de3bb2cc2e9484b71c53e652ec0f
theonlywisdom <WABaah@edinst.com>
2024-11-20 10:17:47 +0000

    test shouldloadfriends added pass

---
+++ FriendStorage.UITests/ViewModel/NavigationDataProviderMock.cs
@@ -0,0 +1,14 @@
+﻿using FriendStorage.DataAccess;
+using FriendStorage.Model;
+
+namespace FriendStorage.UITests.ViewModel
+{
+    public class NavigationDataProviderMock : INavigationDataProvider
+    {
+        public IEnumerable<Friend> GetAllFriends()
+        {
+            yield return new Friend() { Id = 1, FirstName = "Ama" };
+            yield return new Friend() { Id = 2, FirstName = "Akua" };
+        }
+    }
+}
---
+++ FriendStorage.UITests/ViewModel/NavigationViewModelTests.cs
@@ -0,0 +1,38 @@
+﻿using FriendStorage.UI.ViewModel;
+
+namespace FriendStorage.UITests.ViewModel
+{
+    public class NavigationViewModelTests
+    {
+        [Fact]
+        public void ShouldLoadFriends()
+        {
+            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+
+            viewModel.Load();
+
+            Assert.Equal(2, viewModel.Friends.Count);
+
+            var friend = viewModel.Friends.SingleOrDefault(f => f.Id == 1);
+
+            Assert.NotNull(friend);
+            Assert.Equal("Ama", friend.FirstName);
+
+            friend = viewModel.Friends.SingleOrDefault(f => f.Id == 2);
+
+            Assert.NotNull(friend);
+            Assert.Equal("Akua", friend.FirstName);
+        }
+
+        [Fact]
+        public void ShouldLoadFriendsOnlyOnce()
+        {
+            var viewModel = new NavigationViewModel(new NavigationDataProviderMock());
+
+            viewModel.Load();
+            viewModel.Load();
+
+            Assert.Equal(2, viewModel.Friends.Count);
+        }
+    }
+}
