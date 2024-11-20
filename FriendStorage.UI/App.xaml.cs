3db7c6a2c241ead4b4bfdb52b37dce376c3cebd6
theonlywisdom <WABaah@edinst.com>
2024-11-20 11:46:04 +0000

    add di container

--- FriendStorage.UI/App.xaml.cs
+++ FriendStorage.UI/App.xaml.cs
@@ -1,8 +1,8 @@
 ﻿using FriendStorage.DataAccess;
 using FriendStorage.UI.View;
 using FriendStorage.UI.ViewModel;
+using Microsoft.Extensions.DependencyInjection;
 using System.Windows;
-
 namespace FriendStorage.UI
 {
     /// <summary>
@@ -10,17 +10,33 @@ namespace FriendStorage.UI
     /// </summary>
     public partial class App : Application
     {
+        private readonly IServiceProvider _serviceProvider;
+        public App()
+        {
+            ServiceCollection services = new();
+            ConfigureServices(services);
+            _serviceProvider = services.BuildServiceProvider();
+        }
+
+        private void ConfigureServices(ServiceCollection services)
+        {
+            services.AddTransient<MainWindow>();
+            services.AddTransient<MainViewModel>();
+            services.AddSingleton<IDataService, FileDataService>();
+
+            services.AddSingleton<Func<IDataService>>(sp => () => sp.GetRequiredService<IDataService>());
+
+            services.AddTransient<INavigationDataProvider, NavigationDataProvider>();
+            services.AddTransient<INavigationViewModel, NavigationViewModel>();
+        }
+
         protected override void OnStartup(StartupEventArgs e)
         {
             base.OnStartup(e);
 
-            var mainWindow = new MainWindow(new MainViewModel(
+            var mainWindow = _serviceProvider.GetService<MainWindow>();
 
-                new NavigationViewModel(
-                new NavigationDataProvider(
-                    () => new FileDataService()))));
-
-            mainWindow.Show();
+            mainWindow?.Show();
         }
     }
 
