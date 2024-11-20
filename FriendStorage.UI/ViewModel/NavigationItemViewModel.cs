fb7ad71e0bd722cd9e253d7d68287c99cdc283a5
theonlywisdom <WABaah@edinst.com>
2024-11-20 16:37:28 +0000

    add NavigationItemViewModel

---
+++ FriendStorage.UI/ViewModel/NavigationItemViewModel.cs
@@ -0,0 +1,14 @@
+﻿namespace FriendStorage.UI.ViewModel
+{
+    public class NavigationItemViewModel : ViewModelBase
+    {
+        public NavigationItemViewModel(int id, string displayMember)
+        {
+            Id = id;
+            DisplayMember = displayMember;
+        }
+
+        public int Id { get; private set; }
+        public string DisplayMember { get; private set; }
+    }
+}
