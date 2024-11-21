4f531c7aa7b2ac2090c50c71bc5bb779591214f0
theonlywisdom <WABaah@edinst.com>
2024-11-21 16:29:38 +0000

    ShouldRaisePropertyChangedEventForDisplayMember passed

--- FriendStorage.UI/ViewModel/NavigationItemViewModel.cs
+++ FriendStorage.UI/ViewModel/NavigationItemViewModel.cs
@@ -7,6 +7,8 @@ namespace FriendStorage.UI.ViewModel
     public class NavigationItemViewModel : ViewModelBase
     {
         private readonly IEventAggregator _eventAggregator;
+        private string _displayMember;
+
         public NavigationItemViewModel(int id, string displayMember, IEventAggregator eventAggregator)
         {
             _eventAggregator = eventAggregator;
@@ -23,7 +25,14 @@ namespace FriendStorage.UI.ViewModel
         }
 
         public int Id { get; private set; }
-        public string DisplayMember { get; set; }
+        public string DisplayMember
+        {
+            get => _displayMember; set
+            {
+                _displayMember = value;
+                OnPropertyChanged();
+            }
+        }
 
         public ICommand OpenFriendEditViewCommand { get; private set; }
     }
--- FriendStorage.UITests/ViewModel/NavigationItemViewModelTests.cs
+++ FriendStorage.UITests/ViewModel/NavigationItemViewModelTests.cs
@@ -1,28 +1,48 @@
 ﻿using FriendStorage.UI.Events;
 using FriendStorage.UI.ViewModel;
+using FriendStorage.UITests.Extensions;
 using Moq;
 
 namespace FriendStorage.UITests.ViewModel
 {
     public class NavigationItemViewModelTests
     {
+        const int _friendId = 7;
+        private readonly Mock<IEventAggregator> _eventAggregatorMock;
+        private readonly NavigationItemViewModel _viewModel;
+
+        public NavigationItemViewModelTests()
+        {
+            _eventAggregatorMock = new Mock<IEventAggregator>();
+
+            _viewModel = new NavigationItemViewModel(_friendId, "Yaw", _eventAggregatorMock.Object);
+        }
+
         [Fact(DisplayName = nameof(ShouldPublishOpenFriendEditViewEvent))]
         public void ShouldPublishOpenFriendEditViewEvent()
         {
             const int friendId = 7;
             var eventMock = new Mock<OpenFriendEditViewEvent>();
 
-            var eventAggregatorMock = new Mock<IEventAggregator>();
-
-            eventAggregatorMock
+            _eventAggregatorMock
                 .Setup(ea => ea.GetEvent<OpenFriendEditViewEvent>())
                 .Returns(eventMock.Object);
 
-            var viewModel = new NavigationItemViewModel(friendId, "Yaw", eventAggregatorMock.Object);
-
-            viewModel.OpenFriendEditViewCommand.Execute(null);
+            _viewModel.OpenFriendEditViewCommand.Execute(null);
 
             eventMock.Verify(e => e.Publish(friendId), Times.Once);
+        }
+
+        [Fact(DisplayName = nameof(ShouldRaisePropertyChangedEventForDisplayMember))]
+        public void ShouldRaisePropertyChangedEventForDisplayMember()
+        {
+            bool fired = _viewModel.IsPropertyChangedFired(() =>
+              {
+
+                  _viewModel.DisplayMember = "Changed";
+              }, nameof(_viewModel.DisplayMember));
+
+            Assert.True(fired);
         }
     }
 }
